45dfe01a8ec526bfc92a675fe412f6726264505f
Jiangwenzhe <jiangwenzhe2016@gmail.com>
2024-03-15 20:38:41 +0800

    修改拼写错误

--- packages/react-reconciler/src/commitWork.ts
+++ packages/react-reconciler/src/commitWork.ts
@@ -1,4 +1,4 @@
-import { Container, appendChildToContainer } from 'hostConfig';
+import { appendChildToContainer, Container } from 'hostConfig';
 import { FiberNode, FiberRootNode } from './fiber';
 import { MutationMask, NoFlags, Placement } from './fiberFlags';
 import { HostComponent, HostRoot, HostText } from './workTags';
@@ -18,10 +18,11 @@ export const commitMutationEffects = (finishedWork: FiberNode) => {
 		) {
 			nextEffect = child;
 		} else {
-			// 向上遍历
+			// 向上遍历 DFS
 			up: while (nextEffect !== null) {
-				commitMutationEffectOnFiber(nextEffect);
+				commitMutaitonEffectsOnFiber(nextEffect);
 				const sibling: FiberNode | null = nextEffect.sibling;
+
 				if (sibling !== null) {
 					nextEffect = sibling;
 					break up;
@@ -32,25 +33,24 @@ export const commitMutationEffects = (finishedWork: FiberNode) => {
 	}
 };
 
-const commitMutationEffectOnFiber = (finishedWork: FiberNode) => {
+const commitMutaitonEffectsOnFiber = (finishedWork: FiberNode) => {
 	const flags = finishedWork.flags;
+
 	if ((flags & Placement) !== NoFlags) {
 		commitPlacement(finishedWork);
 		finishedWork.flags &= ~Placement;
 	}
 	// flags Update
-
 	// flags ChildDeletion
 };
 
 const commitPlacement = (finishedWork: FiberNode) => {
-	// finishedWork
 	if (__DEV__) {
-		console.warn('执行 placement 操作', finishedWork);
+		console.warn('执行Placement操作', finishedWork);
 	}
 	// parent DOM
 	const hostParent = getHostParent(finishedWork);
-	// 找到 finishedWOrk 对应的 dom
+	// finishedWork ~~ DOM append parent DOM
 	if (hostParent !== null) {
 		appendPlacementNodeIntoContainer(finishedWork, hostParent);
 	}
@@ -71,21 +71,21 @@ function getHostParent(fiber: FiberNode): Container | null {
 		parent = parent.return;
 	}
 	if (__DEV__) {
-		console.warn('未找到 host parent');
+		console.warn('未找到host parent');
 	}
 	return null;
 }
 
 function appendPlacementNodeIntoContainer(
-	finishedWrok: FiberNode,
+	finishedWork: FiberNode,
 	hostParent: Container
 ) {
 	// fiber host
-	if (finishedWrok.tag === HostComponent || finishedWrok.tag === HostText) {
-		appendChildToContainer(hostParent, finishedWrok.stateNode);
+	if (finishedWork.tag === HostComponent || finishedWork.tag === HostText) {
+		appendChildToContainer(hostParent, finishedWork.stateNode);
 		return;
 	}
-	const child = finishedWrok.child;
+	const child = finishedWork.child;
 	if (child !== null) {
 		appendPlacementNodeIntoContainer(child, hostParent);
 		let sibling = child.sibling;
